2c069b48fab7d79ecdeadee378261cfaff9413af
Austin Crinklaw <austincrinklaw@protonmail.com>
2019-06-20 14:00:52 -0700

    Fix MHC g domain path issue

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bio-arc",
-    version="0.0.1",
+    version="0.0.2",
     author="Austin Crinklaw",
     author_email="acrinklaw@lji.org",
     description="Antigen Receptor Classifier",
