4902211dd78272c817551b802c297ec34f65b1f8
Austin Crinklaw <acrinklaw@lji.org>
2021-09-14 13:29:21 -0700

    RTD fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../ARC'))
-autodoc_mock_imports = ['ARC']
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 
